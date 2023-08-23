76cc5e2d17ae618f9a463dd5b91855429f23a8e5
Gabriel Miranda <gbmonovaes@gmail.com>
2023-08-23 18:55:35 -0300

    23/08/2023

--- conceitosBasicos/Class1.cs
+++ conceitosBasicos/Class1.cs
@@ -24,4 +24,24 @@ namespace conceitosBasicos
             return metros * 1000;
         }
     }
+    //Crie o programa CalculaAumento que deve solicitar o valor atual do salário e o percentual de aumento. Ao
+    //final deverá exibir todas as informações: salário inicial, percentual de aumento, valor do aumento e valor
+    //do novo salário
+    public class Calculadora
+    {
+        //double aux;
+        public double CalculaAumento(double inicial, double percentual)
+        {
+            //aux = inicial + (inicial * (percentual / 100));
+            return inicial + (inicial * (percentual / 100));
+
+        }
+        public double CalculaDiferenca(double inicial, double percentual)
+        {
+            return  CalculaAumento(inicial, percentual) - inicial;
+        }
+        
+    }
+   
+    
 }   
--- conceitosBasicos/Program.cs
+++ conceitosBasicos/Program.cs
@@ -1,20 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using conceitosBasicos;
+//using System.Security.Cryptography;
+
+//int num1 = 20;
+//int num2 = 11, result;
+
+
+//Somador soma;
+//soma = new Somador();
+
+//result = soma.Soma(num1, num2);
+//Console.WriteLine($"A soma dos números {num1} + {num2} é {result}");
+
+//Conversor metrosMilimetros;
+//metrosMilimetros = new Conversor();
+
+//double resultm = Conversor.MetrosMilimetros(1.52);
+//double metros = 1.76;
+//Console.WriteLine("A medida {0} metros corresponde a {1} milimetros.",metros, Conversor.MetrosMilimetros(1.72));
+
 using conceitosBasicos;
-using System.Security.Cryptography;
 
-int num1 = 20;
-int num2 = 11, result;
+Console.WriteLine("Insira o salário inicial:");
+double inicial = Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine("Insira o número correspondente aumento percentual (sem o sinal de %):");
+double percentual = Convert.ToDouble(Console.ReadLine());
+double aumento = 0, diferenca;
 
-Somador soma;
-soma = new Somador();
+Calculadora calc;
+calc = new Calculadora();
 
-result = soma.Soma(num1, num2);
-Console.WriteLine($"A soma dos números {num1} + {num2} é {result}");
+aumento = calc.CalculaAumento(inicial, percentual);
+diferenca = calc.CalculaDiferenca(inicial, percentual);
 
-Conversor metrosMilimetros;
-metrosMilimetros = new Conversor();
-
-double resultm = Conversor.MetrosMilimetros(1.52);
-double metros = 1.76;
-Console.WriteLine("A medida {0} metros corresponde a {1} milimetros.",metros, Conversor.MetrosMilimetros(1.72));
+Console.WriteLine($"Um aumento de {percentual}%, sobre o salário de R${inicial}, é {diferenca}." +
+    $" O seu novo salário será {aumento}");
